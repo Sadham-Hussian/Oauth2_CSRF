d38201e62907e713b9fe7544027898f0f494ca92
Sivagiri Visakan <siva.g.visakan@gmail.com>
2021-04-18 16:35:08 +0530

    Add a basic state validation.

--- consumer/app.py
+++ consumer/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session
+from flask.globals import request
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 CORS(app)
 
+PROVIDER_BASE_URL = "http://localhost:5000/provider/oauth2/vulnerable"
+PROVIDER_PROTECTED_BASE_URL = "http://localhost:5000/provider/oauth2/protected"
 @app.route('/')
 def index():
 	return "Hello World"
@@ -21,7 +27,35 @@ def vulnerable_login():
 
 @app.route("/consumer/oauth2/vulnerable/callback", methods=['GET'])
 def vulnerable_callback():
-	return "Callback"
+	code = request.args.get('code')
+	if not code:
+		return {"message": "Invalid code"}, 400
+	get_access_token = requests.post(f'{PROVIDER_BASE_URL}/token', )
+	return get_access_token.json()
+
+
+@app.route('/consumer/oauth2/protected/login', methods=['GET'])
+def protected_login():
+	provider_url = "http://localhost:5000/provider/oauth2/protected/login"
+	redirect_uri = "redirect_uri=http://localhost:4000/consumer/oauth2/protected/callback"
+	client_id = "&client_id=" + "consumer"
+	state = "&state=abcd"
+	session['state'] = 'abcd'
+	return redirect(provider_url + "?" + redirect_uri + client_id + state)
+
+@app.route("/consumer/oauth2/protected/callback", methods=['GET'])
+def protected_callback():
+	code = request.args.get('code')
+	state = request.args.get('state')
+	stored_state = session['state']
+	print(stored_state, state)
+	if not state == stored_state:
+		return {"message": "Invalid request - possible CSRF"}
+	if not code:
+		return {"message": "Invalid code"}, 400
+	get_access_token = requests.post(f'{PROVIDER_BASE_URL}/token', )
+	print(get_access_token.text)
+	return get_access_token.json()
 
 
 if __name__=='__main__':
--- provider/app.py
+++ provider/app.py
@@ -7,6 +7,8 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+DB = {}
+
 @app.route('/')
 def index():
 	return "Oauth provider server"
@@ -39,6 +41,19 @@ def userinfo():
 		"ph_no": "+91-6383440981",
 	}
 	return json.dumps(info)
+
+@app.route("/provider/oauth2/protected/login", methods=['GET','POST'])
+def auth_protected():
+	if request.method == "GET":
+		redirect_uri = request.args.get('redirect_uri')
+		client_id = request.args.get('client_id')
+		return render_template('authorise.html', redirect_uri=redirect_uri)
+	elif request.method == "POST":
+		redirect_uri = request.args.get('redirect_uri')
+		
+		code = ''.join((random.choice(string.ascii_letters + string.digits) for _ in range(32)))
+		return redirect(redirect_uri + '?code=' + code + '&state=abcd')
+
 
 if __name__=='__main__':
 	app.run(debug=True)
